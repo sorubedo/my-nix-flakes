216aa13b98df21f222b9e0ecbc68215da518b584
Sorubedo <zhanrenhangg@gmail.com>
2025-07-23 18:48:13 +0800

    1

--- flake.nix
+++ flake.nix
@@ -4,55 +4,43 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      # 支持的系统架构
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # 获取当前系统的 nixpkgs 实例
+        pkgs = import nixpkgs { inherit system; };
 
-      # 一个为每个系统生成属性的辅助函数
-      forAllSystems = function: nixpkgs.lib.genAttrs supportedSystems (system: function system);
+        # --- 自动发现包的逻辑 (保持不变, 但更清晰) ---
+        # 扫描 ./pkgs 目录下的所有子目录（每个子目录代表一个包）
+        packagesPath = ./pkgs;
+        packageNames = builtins.attrNames (
+          lib.filterAttrs (name: type: type == "directory") (builtins.readDir packagesPath)
+        );
 
-      # --- 自动发现包的魔法 ---
-      # 导入 pkgs 目录，并为每个系统构建其中的所有包
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-      pkgs = forAllSystems (system:
-        let
-          # 过滤出 pkgs 目录下的所有文件夹
-          pkg-names = builtins.attrNames (
-            nixpkgs.lib.filterAttrs (name: type: type == "directory") (builtins.readDir ./pkgs)
-          );
-          # 为每个文件夹（包）调用 callPackage
-          callPackageFor = pkg-name: nixpkgsFor.${system}.callPackage (./pkgs + "/${pkg-name}/package.nix") {};
-        in
-        # 生成 { sing-box = <derivation>; another-package = <derivation>; ... }
-        nixpkgs.lib.genAttrs pkg-names callPackageFor
-      );
+        # 为每个发现的包名调用 callPackage
+        buildPackage = name: pkgs.callPackage "${packagesPath}/${name}/package.nix" {};
 
-    in
-    {
-      # --- 输出 1: Packages ---
-      # 将所有发现的包暴露在 packages.<system> 下
-      # 其他 flake 可以通过 my-flakes.packages.x86_64-linux.sing-box 来引用
-      packages = pkgs;
+        # 生成所有包的属性集, 例如 { sing-box = <derivation>; ... }
+        allPackages = lib.genAttrs packageNames buildPackage;
 
-      # --- 输出 2: Overlays ---
-      # 提供一个方便的 overlay，可以一次性添加所有包
-      overlays.default = final: prev: {
-        # 将我们构建的所有包都添加到 overlay 中
-        # 例如，pkgs.x86_64-linux 包含了为该系统构建的所有包
-        inherit (pkgs.${prev.system}) sing-box; # 你可以手动指定，或者直接用 pkgs.${prev.system}
-      };
+      in
+      {
+        # --- 输出 1: Packages ---
+        # 将为当前系统构建的所有包暴露在 packages 下
+        # 结果: self.packages.x86_64-linux.sing-box
+        packages = allPackages;
 
-      # --- (可选) 输出 3: NixOS 模块 ---
-      # 如果你的某些包有对应的 NixOS 服务模块，可以在这里提供
-#       nixosModules.default = { ... }: {
-#         # 例如，为你的 overlay 创建一个选项
-#         options.my-flakes.enable = lib.mkEnableOption "Enable overlay for my custom packages";
-#         config = lib.mkIf config.my-flakes.enable {
-#           nixpkgs.overlays = [ self.overlays.default ];
-#         };
-#       };
-    };
+        # --- 输出 2: Overlays ---
+        # 提供一个默认的 overlay，它会把我们所有的包添加到一个命名空间下
+        # 这是最重要和最健壮的实现方式
+        overlays.default = final: prev: {
+          # 我们创建一个名为 `myFlakes` 的属性集来存放所有的包
+          # 这样做可以避免与 nixpkgs 中的官方包名冲突，是最佳实践
+          myFlakes = self.packages.${prev.system};
+        };
+      }
+    );
 }
--- overlays/default.nix
+++
@@ -1,9 +0,0 @@
-# ./overlays/default.nix (更优化的版本)
-#
-# 这个文件接收一个参数 `packages`，这个参数将由 flake.nix 传入。
-# `packages` 是一个包含了所有自定义包的属性集。
-final: prev: {
-  # 将 `packages` 里的所有属性（即我们的所有自定义包）
-  # 都添加到最终的包集中。
-  inherit (final.callPackage ../pkgs { }) sing-box;
-}
